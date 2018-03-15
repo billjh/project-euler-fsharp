0f1ef247abe415c4f657cd8169188208fbbb2e7e
billjh <bill.jiangh@gmail.com>
2018-03-15 15:52:27 +0800

    Problem 4: Largest palindrome product

--- p004.fsx
+++ p004.fsx
@@ -1,6 +1,6 @@
 let reverseString (s:string) = s.ToCharArray() |> Array.rev |> System.String
 
-let isPalindrome (x:int) = string(x) = reverseString (string(x))
+let isPalindrome (x:int) = string(x) = (string(x) |> reverseString)
 
 {100 .. 999} 
     |> Seq.collect (fun n -> {n .. 999} |> Seq.map (fun m -> (n * m)))
