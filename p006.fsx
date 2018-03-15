e06971bff74eadc8c0fc0fd34825159e39c09feb
billjh <bill.jiangh@gmail.com>
2018-03-15 15:53:28 +0800

    Problem 6: Sum square difference

--- p006.fsx
+++ p006.fsx
@@ -1,4 +1,5 @@
 let sumOfSquare s = s |> Seq.sumBy (fun x -> x * x)
 let squareOfSum s = s |> Seq.sum |> (fun x -> x * x)
 
-{1 .. 100} |> (fun s -> squareOfSum s - sumOfSquare s)
+{1 .. 100} 
+    |> (fun s -> squareOfSum s - sumOfSquare s)
