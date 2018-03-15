a85301d99507475184d0caeab0b4b0ed6c70b59b
billjh <bill.jiangh@gmail.com>
2018-03-15 15:50:47 +0800

    Problem 2: Even Fibonacci numbers

--- p002.fsx
+++ p002.fsx
@@ -1,4 +1,7 @@
 // seq [1; 2; 3; 5; ...]
 let fibonacciSeq = Seq.unfold (fun (current, next) -> Some(current, (next, current + next))) (1, 2)
 
-fibonacciSeq |> Seq.filter (fun x -> x % 2 = 0) |> Seq.takeWhile (fun x -> x < 4000000) |> Seq.sum
+fibonacciSeq 
+    |> Seq.filter (fun x -> x % 2 = 0) 
+    |> Seq.takeWhile (fun x -> x < 4000000) 
+    |> Seq.sum
