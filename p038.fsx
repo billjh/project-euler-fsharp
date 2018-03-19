4286bcba8b11aab3e756e6939cbab3c9f9abc256
billjh <bill.jiangh@gmail.com>
2018-03-19 16:58:27 +0800

    Problem 38: Pandigital multiples

---
+++ p038.fsx
@@ -0,0 +1,17 @@
+let isPandigital = 
+    String.filter System.Char.IsDigit 
+    >> Seq.toArray 
+    >> Array.sort
+    >> (=) [|'1' .. '9'|]
+
+let findPandigitalProduct n =
+    List.map (fun m -> n * m |> string) [1 .. 9]
+    |> List.scan (+) ""
+    |> List.filter isPandigital
+    |> (fun l -> if l.IsEmpty then None else Some(l.Head))
+
+{2 .. 9999}
+    |> Seq.map findPandigitalProduct
+    |> Seq.filter Option.isSome
+    |> Seq.map (Option.get)
+    |> Seq.maxBy int
