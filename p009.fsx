b3e63762f31351aaa378d655f0598b58a7407512
billjh <bill.jiangh@gmail.com>
2018-03-20 11:37:22 +0800

    Problem 9: Special Pythagorean triplet

--- p009.fsx
+++ p009.fsx
@@ -1,5 +1,5 @@
 // generate seq of tuple (a, b, c) where a <= b <= c and a + b + c = n
-let abcSeq n = {1 .. n / 3} |> Seq.collect (fun a -> {a .. (n - a) / 2} |> Seq.map (fun b -> (a, b, 1000 - a - b)))
+let abcSeq n = {1 .. n / 3} |> Seq.collect (fun a -> {a .. (n - a) / 2} |> Seq.map (fun b -> (a, b, n - a - b)))
 
 let isPythagoreanTriplet (a, b, c) = a * a + b * b = c * c
 
