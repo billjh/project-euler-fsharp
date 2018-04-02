8d08af36846586a9a6fdb258e8fb7a556b2029c1
Huan Jiang <bill.jiangh@gmail.com>
2018-04-02 21:25:40 +0800

    Problem 12: Highly divisible triangular number

--- p012.fsx
+++ p012.fsx
@@ -14,15 +14,15 @@ let rec power b p =
     | 0 -> 1
     | _ -> b * (power b (p - 1))
 
-let rec divisorCount n factor =
-    let currentCount = factorCount n factor
-    match n with
-    | 1 -> 1
-    | _ -> (currentCount + 1) * divisorCount (n / (power factor currentCount)) (nextFakePrime factor)
-
-let totalDivisorCount n = divisorCount n 2
+let divisorCount n = 
+    let rec divisorCountRec n factor =
+        let currentCount = factorCount n factor
+        match n with
+        | 1 -> 1
+        | _ -> (currentCount + 1) * divisorCountRec (n / (power factor currentCount)) (nextFakePrime factor)
+    divisorCountRec n 2
 
 // seq [1; 3; 6; 10; ...]
 let triangularNumbers = Seq.initInfinite id |> Seq.skip 1 |> Seq.map (fun n -> (1 + n) * n / 2)
 
-triangularNumbers |> Seq.find (fun n -> divisorCount n 2 > 500)
+triangularNumbers |> Seq.find (fun n -> divisorCount n > 500)
