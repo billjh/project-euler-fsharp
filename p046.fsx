c960077a1123723ec4643bb0982081fb8f9581fe
billjh <bill.jiangh@gmail.com>
2018-04-02 17:41:42 +0800

    Problem 46: Goldbach's other conjecture

---
+++ p046.fsx
@@ -0,0 +1,21 @@
+// from p003.fsx
+let takeSqrt (n:int64) = n |> double |> System.Math.Sqrt |> int64
+
+// from p003.fsx
+let isPrime (n:int64) = 
+    match n with
+    | n when n < 2L -> false
+    | n when n = 2L -> true
+    | n when n % 2L = 0L -> false
+    | n -> {3L .. 2L .. takeSqrt n} |> Seq.exists (fun (div:int64) -> n % div = 0L) |> not
+
+let oddComposites = Seq.initInfinite id |> Seq.map (int64 >> (fun n -> 3L + n * 2L)) |> Seq.filter (isPrime >> not)
+
+let verifyConjecture n =
+    Seq.initInfinite id
+        |> Seq.skip 1
+        |> Seq.map int64
+        |> Seq.takeWhile (fun p -> 2L * p * p <= n)
+        |> Seq.exists (fun p -> isPrime (n - 2L * p * p))
+
+oddComposites |> Seq.find (verifyConjecture >> not)
