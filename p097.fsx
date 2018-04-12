6f170f9917cf47ce5c05b4a8f42b5a273b66395e
billjh <bill.jiangh@gmail.com>
2018-04-12 10:26:50 +0800

    Problem 97: Large non-Mersenne prime

--- p097.fsx
+++ p097.fsx
@@ -4,8 +4,8 @@ let mul a b = (a * b) |> keepLastTenDigits
 
 let add a b = (a + b) |> keepLastTenDigits
 
-{ 1 .. 7830457 } 
-    |> Seq.map (fun _ -> 2L)
+2L
+    |> Seq.replicate 7830457
     |> Seq.fold mul 1L
     |> (mul) 28433L
     |> (add) 1L
