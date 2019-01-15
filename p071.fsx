ff6efab47c837f3e4c2140499ed7f54012e8a35a
billjh <bill.jiangh@gmail.com>
2019-01-15 14:51:52 +0800

    Problem 71: Ordered fractions

---
+++ p071.fsx
@@ -0,0 +1,16 @@
+// from p005.fsx modifed
+let rec gcd x y = 
+    match (x, y) with
+    | (x, y) when x < y -> gcd y x
+    | (x, y) when y = 0 -> x
+    | (x, y) -> gcd y (x % y)
+
+type Fraction = { numerator:int ; denominator:int }
+
+let isReducedForm f = gcd f.numerator f.denominator = 1
+
+{8 .. 1000000}
+    |> Seq.map (fun d -> { Fraction.numerator = (3.0/7.0 * float d) |> floor |> int ; denominator = d })
+    |> Seq.filter isReducedForm
+    |> Seq.maxBy (fun f -> (float f.numerator) / (float f.denominator))
+    |> (fun f -> f.numerator)
