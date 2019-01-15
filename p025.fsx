1d7d1c82936adc1fb799ea2f6eaa2b2847d9139a
billjh <bill.jiangh@gmail.com>
2019-01-15 15:05:57 +0800

    Problem 25: 1000-digit Fibonacci number

--- p025.fsx
+++ p025.fsx
@@ -1,4 +1,4 @@
-// from p002.fsx modifed
+// from p002.fsx modified
 let fibonacciSeq = Seq.unfold (fun (current, next) -> Some(current, (next, current + next))) (0I, 1I)
 
 fibonacciSeq |> Seq.findIndex (string >> String.length >> (fun l -> l = 1000))
